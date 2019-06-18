49685ca88c5eaf63b9f838a4475ecc87e9711f5f
Awa Melvine <melvineawa@gmail.com>
2019-06-18 13:33:12 +0100

    Dispatch redux action to add todo item

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addTodo } from "../actions/todoActions";
 
-export default class TodoForm extends Component {
+class TodoForm extends Component {
+  state = {
+    value: ""
+  };
+  submit = event => {
+    event.preventDefault();
+    this.props.addTodo(this.state.value);
+    this.setState({ value: "" });
+  };
   render() {
+    const { value } = this.state;
     return (
       <div>
-        <form>
-          <input name="value" type="text" />
+        <form onSubmit={event => this.submit(event)}>
+          <input
+            type="text"
+            name="value"
+            value={value}
+            onChange={event => this.setState({ value: event.target.value })}
+            placeholder="Add task..."
+          />
           <button type="submit">Add Todo</button>
         </form>
       </div>
     );
   }
 }
+export default connect(
+  null,
+  { addTodo }
+)(TodoForm);
--- todo/src/reducers/todoReducers.js
+++ todo/src/reducers/todoReducers.js
@@ -3,7 +3,7 @@ import { ADD_TODO, TOGGLE_COMPLETE } from "../actions/types";
 export default (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, ...action.payload];
+      return [...state, action.payload];
 
     case TOGGLE_COMPLETE:
       return state.map(todo => {
