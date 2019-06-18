83f6dd82ddaa290f7312729b9d6a331d23707d09
Awa Melvine <melvineawa@gmail.com>
2019-06-18 15:04:03 +0100

    Implement delete todo case in todo reducer

--- todo/src/reducers/todoReducers.js
+++ todo/src/reducers/todoReducers.js
@@ -1,10 +1,11 @@
-import { ADD_TODO, TOGGLE_COMPLETE } from "../actions/types";
+import { ADD_TODO, TOGGLE_COMPLETE, DELETE_TODO } from "../actions/types";
 
 export default (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
       return [...state, action.payload];
-
+    case DELETE_TODO:
+      return state.filter(todo => todo.id !== action.payload);
     case TOGGLE_COMPLETE:
       return state.map(todo => {
         if (todo.id === action.payload) {
